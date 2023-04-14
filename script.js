d8d167091603f2f6bd67b76764598d3ab7181cb0
Augustin C Benny <84260365+augustincbenny@users.noreply.github.com>
2023-04-14 23:48:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function buttonclick(val)
+{
+
+    document.getElementById("screen").value+=val
+}
+function cleardisplay(){
+    document.getElementById("screen").value=""
+}
+function equalClick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById('screen').value=result
+}     
